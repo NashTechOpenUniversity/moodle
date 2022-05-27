dbc7282b9ac6c2a602fe0b5a20464e7eba22a5ce
Thong Bui <qktc1422@gmail.com>
2022-05-27 14:01:23 +0700

    MDL-74766 atto_editor: Fix validation in html source mode

---
+++ lib/editor/atto/plugins/html/lib.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Atto text editor html plugin lib.
+ *
+ * @package    atto_html
+ * @copyright  2022 The Open University.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Initialise the strings required for JS.
+ *
+ * @return void
+ */
+function atto_html_strings_for_js(): void {
+    global $PAGE;
+    $PAGE->requires->strings_for_js(['err_required'], 'form');
+}
+
