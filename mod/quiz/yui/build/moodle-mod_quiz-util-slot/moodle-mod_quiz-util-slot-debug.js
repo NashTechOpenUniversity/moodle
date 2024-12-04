9151ce45a45f9d92dc7488f433143c65696d03c9
hieuvu <hieuvu@nashtechglobal.com>
2024-12-04 09:14:25 +0700

    MDL-82302 quiz: Fix code review

--- mod/quiz/yui/build/moodle-mod_quiz-util-slot/moodle-mod_quiz-util-slot-debug.js
+++ mod/quiz/yui/build/moodle-mod_quiz-util-slot/moodle-mod_quiz-util-slot-debug.js
@@ -113,6 +113,8 @@ Y.Moodle.mod_quiz.util.slot = {
 
     /**
      * Updates the slot number for the provided slot.
+     * If you want to update the code, you also need to update
+     * make_slot_display_number_in_place_editable in mod/quiz/classes/structure.php
      *
      * @method setNumber
      * @param slot {Node} The slot to update the number for.
@@ -122,14 +124,15 @@ Y.Moodle.mod_quiz.util.slot = {
     setNumber: function(slot, number) {
         var yui = this;
         var numberNode = slot.one(yui.SELECTORS.NUMBER)._node;
-        // Using promise causing some delay so it should better store the current slot number to HTML.
-        // So that we can reuse the current slot later without need to wait for promise to completed.
+        // We store the number on a data attribute on the containing span, so it is always available,
+        // even when the inplace editable is being re-rendered asynchronously via a promise.
         numberNode.dataset.currentslotnumber = number;
         require(['core/inplace_editable', 'core/templates', 'core/str', 'core/notification'],
             function(InplaceEditable, Templates, str, Notification) {
                 var inplaceElement = InplaceEditable.getInplaceEditable(numberNode);
-                // We don't need to change custom number.
+                // If slot already have a customised number, then we shouldn't re-render inplace editable element.
                 if (inplaceElement && !numberNode.dataset.customnumber) {
+                    // Get the required strings for the template.
                     str.get_strings([
                         {key: 'edit_slotdisplaynumber_hint', component: 'mod_quiz'},
                         {key: 'edit_slotdisplaynumber_label', component: 'mod_quiz', param: number},
@@ -149,12 +152,13 @@ Y.Moodle.mod_quiz.util.slot = {
                             },
                             linkeverything: true,
                         };
-                        return Templates.render('core/inplace_editable', context).then(function(html, js) {
-                            Templates.replaceNodeContents(
-                                numberNode,
-                                '<span class="accesshide">' + yui.CONSTANTS.QUESTION + '</span>' + html, js);
-                            return true;
-                        });
+                        return Templates.render('core/inplace_editable', context);
+                    }).then(function(html, js) {
+                        // Replace the existing node with the new inplace editable element that have a new slot number.
+                        Templates.replaceNodeContents(
+                            numberNode,
+                            '<span class="accesshide">' + yui.CONSTANTS.QUESTION + '</span>' + html, js);
+                        return true;
                     }).catch(Notification.exception);
                 }
             }
