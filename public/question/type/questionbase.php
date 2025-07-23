3eb1a94506f7f241ed25692c19e9c4239bf5dbeb
Khoa Nguyen Dang <khoauth@gmail.com>
2025-07-23 12:11:29 +0700

    MDL-85510 Questions: clearfix from MDL-84708 affects spacing
    Co-authored-by: Mikel Martín Corrales <mikel@moodle.com>

--- public/question/type/questionbase.php
+++ public/question/type/questionbase.php
@@ -459,14 +459,36 @@ abstract class question_definition {
 
     /** @return the result of applying {@link format_text()} to the question text. */
     public function format_questiontext($qa) {
-        return html_writer::tag('div', $this->format_text($this->questiontext, $this->questiontextformat,
-            $qa, 'question', 'questiontext', $this->id), ['class' => 'clearfix']);
+        $formattext = $this->format_text(
+            $this->questiontext,
+            $this->questiontextformat,
+            $qa,
+            'question',
+            'questiontext',
+            $this->id
+        );
+        if ($formattext) {
+            return html_writer::tag('div', $formattext, ['class' => 'clearfix']);
+        }
+
+        return "";
     }
 
     /** @return the result of applying {@link format_text()} to the general feedback. */
     public function format_generalfeedback($qa) {
-        return html_writer::tag('div', $this->format_text($this->generalfeedback, $this->generalfeedbackformat,
-                $qa, 'question', 'generalfeedback', $this->id), ['class' => 'clearfix']);
+        $formatext = $this->format_text(
+            $this->generalfeedback,
+            $this->generalfeedbackformat,
+            $qa,
+            'question',
+            'generalfeedback',
+            $this->id
+        );
+        if ($formatext) {
+            return html_writer::tag('div', $formatext, ['class' => 'clearfix']);
+        }
+
+        return "";
     }
 
     /**
